50011f3949ac4168580afd9e20ea3ad94948a157
Sergine Winner <serginew1509@gmail.com>
2024-05-18 12:59:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, dolore!</p>
+    <ul>
+        <li>satu</li>
+        <li>satu</li>
+        <li>satu</li>
+    </ul>
 </body>
 </html>
